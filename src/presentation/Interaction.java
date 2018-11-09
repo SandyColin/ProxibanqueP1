1e52b931629b8ca2ec380b765a6636bd2c17513e
SandyC <sandycolin1990@gmail.com>
2018-11-09 14:30:24 +0100

    remove

--- src/presentation/Interaction.java
+++ src/presentation/Interaction.java
@@ -21,17 +21,17 @@ public class Interaction {
 	
 	public void mainMenu() {
 		this.display("Que faire ?");
-		this.display("1. Crï¿½er un client");
-		this.display("2. Gï¿½rer un client");
+		this.display("1. Créer un client");
+		this.display("2. Gérer un client");
 		this.display("3. Sortir du programme");
 	}
 	
 
 	public void mainMenu2() {
-	        this.display("1. Lire les donnï¿½es du client");
-	        this.display("2. Modifier les donnï¿½es d'un client");
+	        this.display("1. Lire les données du client");
+	        this.display("2. Modifier les données d'un client");
 	        this.display("3. Faire un virement");
-	        this.display("4. Simuler un crï¿½dit");
+	        this.display("4. Simuler un crédit");
 	        this.display("5. Supprimer un client");
 	        this.display("6. Revenir au menu prÃ©cÃ©dent");
 	        this.display("7. Sortir du programme");
@@ -39,10 +39,20 @@ public class Interaction {
 	
 	public void mainMenu3() {
         this.display("Que faire ?");
-        this.display("1. Simuler un crï¿½dit immobilier");
-        this.display("2. Simuler un crï¿½dit ï¿½ la consommation");
-        this.display("3. Revenir au menu prÃ©cÃ©dent");
+        this.display("1. Simuler un crédit immobilier");
+        this.display("2. Simuler un crédit à la consommation");
+        this.display("3. Revenir au menu précédent");
         this.display("4. Sortir du programme");
     }
-	
+	public void changeMenu3() {
+        this.display("Que voulez-vous modifier ?");
+        this.display("1. Prénom");
+        this.display("2. Nom");
+        this.display("3. Adresse");
+        this.display("4. Code Postal");
+        this.display("5. Ville");
+        this.display("6. Téléphone");
+        this.display("7. Revenir au menu principal");
+        
+    }
 }
