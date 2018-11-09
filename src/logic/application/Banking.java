0281aa0a2c6338a13a9639d9dda4f466ade8c5a1
SandyC <sandycolin1990@gmail.com>
2018-11-09 12:59:11 +0100

    sandy

--- src/logic/application/Banking.java
+++ src/logic/application/Banking.java
@@ -18,6 +18,7 @@ public class Banking {
 	public Interaction interaction ;
 	public AgencyNetwork agencyNetwork;
 	public boolean running = true;
+	public Client client;
 	
 public Banking() {
 	this.interaction = new Interaction();
@@ -38,7 +39,10 @@ public Banking() {
 		this.agencyNetwork.agencies.add(new Agency ("81190", "02/03/1990"));
 		this.agencyNetwork.agencies.get(0).personalAdvisors.add(new PersonalAdvisor("Banquier"));
 		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client("Sarah", "Ourabah", "avenue de la Paix", 75000, "Paris", "06.07.08.09.10"));
-		
+		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client("Sandy","Colin","rue St Marc",93800, "Epinay", "032648972"));
+		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client("Sylvie","Ritter","rue St 24",93430, "Villetaneuse", "032648972"));
+		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client("Marc","Part","rue Marc",95600, "Eaubonne", "032648972"));
+		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client("Jean","Bon","rue St Leu",95560, "Baillet-en-France", "032848972"));
 	}
 	
 	public void start() {
@@ -57,7 +61,6 @@ public Banking() {
 		String lastname = this.interaction.readData();
 		this.interaction.display("Entrez le pr�nom :");
 		String firstname = this.interaction.readData();
-
 		this.interaction.display("Entrez l'adresse :");
 		String adress = this.interaction.readData();
 		this.interaction.display("Entrez le code postal :");
@@ -66,26 +69,29 @@ public Banking() {
 		String city = this.interaction.readData();
 		this.interaction.display("Entrez le t�l�phone :");
 		String tel = this.interaction.readData();
-		PersonalAdvisor personalAdvisor = new PersonalAdvisor();
 		this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients.add(new Client(lastname, firstname, adress, zipCode, city, tel));
 		System.out.println(firstname + " " + lastname + " habitant "+adress + " "+zipCode + " "+city + " et ayant pour num�ro"+ tel + " a bien �t� ajout�.");
 
 		;
 	}
 	public void clientRemove() {
-
+		PersonalAdvisor personalAdvisor = this.agencyNetwork.agencies.get(0).personalAdvisors.get(0);
+		List<Client> clients = personalAdvisor.clients;
+		this.interaction.display("Etes vous sur de vouloir supprimer ce client ? O/N"); 
+		String sup = this.interaction.readData();
+		if (sup.equals("O")) {
+			clients.remove(this.client);
+		}
 		
-		if (client.currentAccount.equals(Account)) {
-		this.deleteAccount();}
-        this.unableCreditCard();
+		//if (client.currentAccount.equals(Account)) {
+		//this.deleteAccount();}
+       // this.unableCreditCard();
     }
     
     public void deleteAccount() {
-        Map <cl�, Client> delete = new HashMap<>();
         
-		client.lastname = this.interaction.readData();
-		
-		client.firstname = this.interaction.readData();
+        
+
     }
     
     public void unableCreditCard() {
@@ -124,17 +130,18 @@ public Banking() {
 	public void exitProgram() {
 		this.running = false;
 	}
-	
-	public void selectClient() {
-		// afficher liste
-		 
-		List<Client> clients = this.agencyNetwork.agencies.get(0).personalAdvisors.get(0).clients;
+	public void displayListClient() {
+		PersonalAdvisor personalAdvisor = this.agencyNetwork.agencies.get(0).personalAdvisors.get(0);
+		List<Client> clients = personalAdvisor.clients;
 		for (Client client : clients) {
 			int index = clients.indexOf(client);
 			this.interaction.display(index + " - "+ client);}
 		this.interaction.display("Tapez le numéro associé au client concerné");
-	    String userInputIndex = this.interaction.readData();	
-		
+	    int userInputIndex = Integer.parseInt(this.interaction.readData());
+	    this.client = clients.get(userInputIndex);
+	}
+	public void selectClient() { 
+		this.displayListClient();
 		this.interaction.mainMenu2();
 		this.interaction.display("Entrez votre choix: ");
 	     String userInput = this.interaction.readData();
@@ -167,7 +174,7 @@ public Banking() {
             } else if (userInput.equals("2")) {
                 this.consumptionCreditSimulation();
             } else if (userInput.equals("3")) {
-                this.manageClient();
+                this.selectClient();
             } else if (userInput.equals("4")) {
      			this.exitProgram();
          }
